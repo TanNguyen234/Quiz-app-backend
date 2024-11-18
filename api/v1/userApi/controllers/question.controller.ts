32c156f72ec58490e035709b16206a39e271a7cd
TanNguyen234 <tanntd.2005@gmail.com>
2024-11-18 19:14:01 +0700

    lay topic 2 và trang thái

--- api/v1/userApi/controllers/question.controller.ts
+++ api/v1/userApi/controllers/question.controller.ts
@@ -7,34 +7,40 @@ export const index = async (req: Request, res: Response) => {
     const id: String = req.params.id
 
     if(id) {
-        const topic = await Topic.findOne({
-            _id: id,
-            deleted: false,
-            status: "active"
-        })
-
-        if(topic) {
-            const data = await Question.find({
-                topicId: id,
-                status: 'active',
-                deleted: false
-            });
-            
-            res.json({
-                topic: topic,
-                code: 200,
-                data: data
+        try {
+            const topic = await Topic.findOne({
+                _id: id,
+                deleted: false,
+                status: "active"
             })
-        } else {
+    
+            if(topic) {
+                const data = await Question.find({
+                    topicId: topic._id,
+                    deleted: false,
+                    status: "active"
+                })
+
+                res.json({
+                    topic: topic,
+                    code: 200,
+                    data: data
+                })
+
+                return;
+            }
+        } catch (error) {
             res.json({
                 code: 400,
                 message: 'Invalid ID'
             })
+            return;
         }
     } else {
         res.json({
             code: 400,
             message: 'Invalid ID'
         })
+        return;
     }
 }
