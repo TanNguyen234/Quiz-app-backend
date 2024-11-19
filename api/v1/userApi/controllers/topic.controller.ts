d046739089ea6984b96086741cf5377e0367a8ae
TanNguyen234 <tanntd.2005@gmail.com>
2024-11-19 14:11:58 +0700

    lay mot topic

--- api/v1/userApi/controllers/topic.controller.ts
+++ api/v1/userApi/controllers/topic.controller.ts
@@ -2,14 +2,36 @@ import { Request, Response } from "express"
 import Topic from "../models/topic.model"
 
 //[GET] /api/v1/topics
-export const index = async (req: Request, res: Response) => {
-    const data = await Topic.find({
-        status: 'active',
-        deleted: false
-    });
+export const index = async (req: Request, res: Response): Promise<void> => {
+    const id: any = req.query.id;
 
-    res.json({
-        code: 200,
-        data: data
-    })
+    if(id) {
+        const data = await Topic.findOne({
+            _id: id,
+            status: 'active',
+            deleted: false
+        });
+
+        if(data) {
+            res.json({
+                code: 200,
+                data: data
+            })
+        } else {
+            res.json({
+                code: 400,
+                message: "Invalid id"
+            })
+        }
+    } else {
+        const data = await Topic.find({
+            status: 'active',
+            deleted: false
+        });
+
+        res.json({
+            code: 200,
+            data: data
+        })
+    }
 }
