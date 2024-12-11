b71fa8e48387bbca8e9d63d0c3897344bd85ed77
TanNguyen234 <tanntd.2005@gmail.com>
2024-12-11 17:49:24 +0700

    Typing

--- api/v1/userApi/socket/client/chat.socket.ts
+++ api/v1/userApi/socket/client/chat.socket.ts
@@ -1,33 +1,41 @@
-import { Socket } from "socket.io"
+import { Socket } from "socket.io";
 import { handleSendMessage } from "./chat-function";
 
 const chatSocket = async (user: any): Promise<void> => {
-    const io = (global as any)._io;
-    io.once('connection', (socket: Socket) => {
-        console.log("New client connected:", socket.id);
-        try {
-            const { id, fullName } = user
-            //CLIENT_ADD_FRIEND
-            socket.removeAllListeners("CLIENT_SEND_MESSAGE");
-            socket.on('CLIENT_SEND_MESSAGE', async (content) => {
-                console.log('CLIENT_SEND_MESSAGE')
-                const message = await handleSendMessage(id, fullName, content)
-                io.emit("SERVER_RETURN_MESSAGE", message);
-            })
-            
-        } catch (error) {
-            console.log("error: ", error)
-        }
+  const io = (global as any)._io;
+  io.once("connection", (socket: Socket) => {
+    console.log("New client connected:", socket.id);
+    const { id, fullName } = user;
 
-        socket.on("disconnect", () => {
-            console.log("Client disconnected:", socket.id);
-            socket.removeAllListeners('CLIENT_SEND_MESSAGE')
-        });
+    //CLIENT_ADD_FRIEND
+    socket.removeAllListeners("CLIENT_SEND_MESSAGE");
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      console.log("CLIENT_SEND_MESSAGE");
+      const message = await handleSendMessage(id, fullName, content);
+      io.emit("SERVER_RETURN_MESSAGE", message);
+    });
 
-        socket.on("connect_error", (err) => {
-            console.error("Connection error:", err);
-        });
-    })
-}
+    //CLIENT_SEND_MESSAGE
+    socket.removeAllListeners("CLIENT_SEND_TYPING");
+    socket.on("CLIENT_SEND_TYPING", (type) => {
+      console.log("CLIENT_SEND_TYPING", type);
 
-export default chatSocket
+      socket.broadcast.emit("SERVER_RETURN_TYPING", {
+        id,
+        fullName,
+        type
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected:", socket.id);
+      socket.removeAllListeners("CLIENT_SEND_MESSAGE");
+    });
+
+    socket.on("connect_error", (err) => {
+      console.error("Connection error:", err);
+    });
+  });
+};
+
+export default chatSocket;
